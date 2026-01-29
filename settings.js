ce2aa89dc7da69f2c9343c29917276d8782f05e0
Finjohns <kelvinonginjo08@gmail.com>
2026-01-30 00:33:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'Jinwiil', // Your name
   ownerNumber: '25469365617', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.2.4",
 };
